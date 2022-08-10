b3b34821dc7cf7aea748482254ad32297e7a2ed1
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-08-10 01:39:46 +0100

    second commit

---
+++ training.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('#nav')
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 80) {
+      navbar.classList.add("navbar-fixed");
+    } else {
+      navbar.classList.remove("navbar-fixed");
+    }
+  });
